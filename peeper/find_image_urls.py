fcdce90c7a8acd70e83b6a236371f52bb7b02454
Matt Luongo <mhluongo@gmail.com>
2011-10-28 12:54:50 -0400

    Further function renames.

--- peeper/find_image_urls.py
+++ peeper/find_image_urls.py
@@ -5,7 +5,7 @@ from urlparse import urljoin
 def find_image_urls(url=None, html=None, duplicate=False, alphabetical=False):
   assert url is not None or html is not None
   if html == "": return []
-  urls = __find_image_urls(url, html)
+  urls = _find_image_urls(url, html)
   if not duplicate: urls = uniquify(urls)
   if alphabetical: urls.sort()
   return urls
@@ -14,15 +14,15 @@ def uniquify(seq):
   seen = set()
   return [x for x in seq if x not in seen and not seen.add(x)]
 
-def __find_image_urls(url, html):
-  tree = __tree(url, html)
-  base = __base(tree, url)
+def _find_image_urls(url, html):
+  tree = get_tree(url, html)
+  base = get_base(tree, url)
   return [
     urljoin(base, x.attrib['src'])
     for x in tree.xpath('//img')
     if 'src' in x.attrib ]
 
-def __tree(url, html):
+def get_tree(url, html):
   parser = etree.HTMLParser()
   if html is None:
     response = mechanize.Browser().open(url)
@@ -30,17 +30,17 @@ def __tree(url, html):
   else:
     return etree.fromstring(text=html, parser=parser, base_url=url)
 
-def __base(tree, url):
+def get_base(tree, url):
   for base in tree.xpath('//base'):
     if 'href' in base.attrib:
-      return __urljoin(base.attrib['href'], url)
+      return _urljoin(base.attrib['href'], url)
   return url
 
-def __urljoin(href, url=None):
+def _urljoin(href, url=None):
   if url is None: return href
   else: return urljoin(url, href)
 
-def __find_image_urlsArgParser():
+def get_arg_parser():
   parser = argparse.ArgumentParser()
   parser.add_argument('-u', '--url', type=str)
   parser.add_argument('-d', '--duplicate', action='store_true')
@@ -49,5 +49,5 @@ def __find_image_urlsArgParser():
   return parser
 
 if __name__ == '__main__':
-  args = __find_image_urlsArgParser().parse_args()
+  args = get_arg_parser().parse_args()
   print '\n'.join(find_image_urls(**vars(args)))
